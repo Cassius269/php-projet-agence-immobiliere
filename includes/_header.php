8d429c8864f2a8dde1d53f9ba3e31bf55b94666b
Cassius269 <fahamygaston@gmail.com>
2025-08-04 15:17:46 +0200

    premier commit

---
+++ includes/_header.php
@@ -0,0 +1,13 @@
+<header>
+    <ul>
+        <li>
+            <a href="#">Houses</a>
+        </li>
+        <li>
+            <a href="#">Appartement</a>
+        </li>
+        <li>
+            <a href="#">Login</a>
+        </li>
+    </ul>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Page d'accueil du site officiel de Find My Dream Home">
+   <!-- Intégration de Bootstrap au projet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous"> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script> 
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <?php require_once 'includes/_header.php' ?>
+    <main>
+
+    </main>
+    <?php require_once 'includes/_footer.php' ?>
+</body>
+</html>
